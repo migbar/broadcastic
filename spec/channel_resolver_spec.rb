c3417fabe911ccf3cf225d499a2112b0474bb695
Miguel Barcos <miguel@galar.com>
2013-06-05 16:22:58 +0200

    Tighten up specs.

--- spec/channel_resolver_spec.rb
+++ spec/channel_resolver_spec.rb
@@ -4,34 +4,29 @@ module Broadcastic
 	describe ChannelResolver do
 
 		it "gets the channels from the String channel if the destination type is a string" do
-			resource = stub
 			StringChannel.should_receive(:channels)
-			ChannelResolver.channels_for(resource, {to: "some_string"})
+			ChannelResolver.channels_for(stub, {to: "some_string"})
 		end
 
 		it "gets the channels from the Symbol channel if the destination type is a symbol" do
-			resource = stub
 			SymbolChannel.should_receive(:channels)
-			ChannelResolver.channels_for(resource, {to: :some_symbol})
+			ChannelResolver.channels_for(stub, {to: :some_symbol})
 		end
 
 		it "gets the channels from the Array channel if the destination type is a array" do
-			resource = stub
 			ArrayChannel.should_receive(:channels)
-			ChannelResolver.channels_for(resource, {to: [:some_symbol]})
+			ChannelResolver.channels_for(stub, {to: [:some_symbol]})
 		end
 
 		it "gets the channels from the NilClass channel if the destination type is empty" do
-			resource = stub
 			NilClassChannel.should_receive(:channels)
-			ChannelResolver.channels_for(resource, {})
+			ChannelResolver.channels_for(stub, {})
 		end
 
 		it "gets the channels from the Object channel if the destination type is a plain object" do
-			resource = stub
 			some_object = stub
 			ObjectChannel.should_receive(:channels)
-			ChannelResolver.channels_for(resource, {to: some_object})
+			ChannelResolver.channels_for(stub, {to: some_object})
 		end
 
 	end
